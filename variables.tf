57e90fed862d548739bea66cb02146dd771a1a2c
aidynilyas <aidynilyas@gmail.com>
2025-06-09 17:58:20 -0400

    variables updated

--- variables.tf
+++ variables.tf
@@ -9,7 +9,7 @@ variable "aws_region" {
 variable "cluster_name" {
   description = "The name of the EKS cluster."
   type        = string
-  default     = "my-eks-dashboard-cluster"
+  default     = "my-eks-cluster"
 }
 
 variable "instance_type" {
